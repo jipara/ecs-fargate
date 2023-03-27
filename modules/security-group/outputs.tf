489f0f728c8af7b2b399e7b425ffb857a6f90286
jipara <zhypargul.esengulova@gmail.com>
2023-03-27 14:51:38 -0700

    remove output

--- modules/security-group/outputs.tf
+++ modules/security-group/outputs.tf
@@ -3,7 +3,7 @@ output "lb_sg_id" {
   value       = aws_security_group.lb.id
 }
 
-output "task_sg_id" {
-  description = "ID of the task security group"
-  value       = aws_security_group.task.id
-}
+# output "task_sg_id" {
+#   description = "ID of the task security group"
+#   value       = aws_security_group.task.id
+# }
