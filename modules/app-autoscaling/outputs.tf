0245120e093df51cf30ba329862cbc311489541e
jipara <50920745+jipara@users.noreply.github.com>
2023-03-28 15:19:57 -0700

    Update outputs.tf

--- modules/app-autoscaling/outputs.tf
+++ modules/app-autoscaling/outputs.tf
@@ -1,7 +1,7 @@
 output "appautoscaling_target_id" {
-  value = aws_appautoscaling_target.dev_to_target.id
+  value = aws_appautoscaling_target.target.id
 }
 
 output "appautoscaling_policy_arn" {
-  value = aws_appautoscaling_policy.dev_to_cpu.arn
+  value = aws_appautoscaling_policy.policy.arn
 }
