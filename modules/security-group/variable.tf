a99ef970cc5e1b2b09563e502aaec265a5508515
jipara <50920745+jipara@users.noreply.github.com>
2023-03-28 15:24:43 -0700

    Update variable.tf

--- modules/security-group/variable.tf
+++ modules/security-group/variable.tf
@@ -4,7 +4,7 @@ variable "lb_sg_name" {
   default     = "example-alb-security-group"
 }
 
-variable "task_sg_name" {
+variable "ecs_sg_name" {
   description = "Name of the task security group"
   type        = string
   default     = "example-task-security-group"
@@ -12,5 +12,10 @@ variable "task_sg_name" {
 
 variable "vpc_id" {
   description = "ID of the VPC"
+  type        = string
+}
+
+variable "task_sg_id" {
+  description = "Name of the task security group"
   type        = string
 }
