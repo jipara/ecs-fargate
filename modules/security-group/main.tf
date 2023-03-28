899f1c281470be6a8aff84621186945e939d33d8
jipara <50920745+jipara@users.noreply.github.com>
2023-03-28 15:24:01 -0700

    Update main.tf

--- modules/security-group/main.tf
+++ modules/security-group/main.tf
@@ -1,6 +1,6 @@
 resource "aws_security_group" "lb" {
   name        = var.lb_sg_name
-  vpc_id      = module.my_vpc.vpc_id
+  vpc_id      = var.vpc_id
 
   ingress {
     protocol    = "tcp"
@@ -16,3 +16,24 @@ resource "aws_security_group" "lb" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+resource "aws_security_group" "ecs_task_sg" {
+  name        = var.ecs_sg_name
+  vpc_id      = var.vpc_id
+
+  ingress {
+    protocol    = "tcp"
+    from_port   = 8081
+    to_port     = 8081
+    security_groups = [aws_security_group.lb.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
