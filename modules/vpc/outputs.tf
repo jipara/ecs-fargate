319eaee702d01736405c5a6efdfba05b0a5fb212
jipara <50920745+jipara@users.noreply.github.com>
2023-03-28 15:25:52 -0700

    Update outputs.tf

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -1,3 +1,11 @@
 output "vpc_id" {
   value = aws_vpc.default.id
 }
+
+output "aws_subnet_pub_id" {
+    value = aws_subnet.public.*.id
+}
+
+output "aws_subnet_private_id" {
+    value = aws_subnet.private.*.id
+}
